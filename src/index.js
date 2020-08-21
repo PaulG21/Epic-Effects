a4ae33cb74b22e1bc3db642c6bdf44873d8fd2c2
Paul Gonzalez <paulgonzalez21@live.com>
2020-08-21 14:18:20 -0400

    splash page animations and button animations

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// bubbly-button
+
+let animateButton = function (e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+
+  e.target.classList.add('animate');
+  setTimeout(function () {
+    e.target.classList.remove('animate');
+  }, 700);
+};
+
+let bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+
+// card flip
+
+const revealButtons = document.querySelectorAll(".reveal-button");
+const hideButtons = document.querySelectorAll(".hide-button");
+
+revealButtons.forEach(revealButton =>
+  revealButton.addEventListener("click", (e) => {
+  const card = e.target.parentElement.parentElement;
+  card.style.transform = "rotateY(180deg)";
+}));
+
+hideButtons.forEach(hideButton => 
+  hideButton.addEventListener("click", (e) => {
+    const cardHide = e.target.parentElement.parentElement;
+    cardHide.style.transform = "rotateY(0deg)";
+}));
+
+
+// Logo lenght algorithm 
+
+const logo = document.querySelectorAll("#logo path");
+for(let i = 0; i<logo.length; i++){
+  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
+};
+
